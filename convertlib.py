36dc6aa411c56186ccc77390dcd6dec574c43602
David Pattison <davidpattison@david-pattisons-mac-pro.local>
2010-11-01 23:16:21 -0700

    it works

--- convertlib.py
+++ convertlib.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import shutil
 
 def everything_between(content, begin, end):
     idx1=content.find(begin)
@@ -22,13 +23,23 @@ def get_title(html_file):
 def get_key_from_title(title):
 	return title[0].lower()
 
+def create_files(targetdirs):
+	base = 'nookipeda'
+	os.mkdir(base)
+	for key in targetdirs:
+		print "mkdir -p key"
+		os.mkdir(os.path.join(base, key))
+		for title, file_name in targetdirs[key]:
+			print "cp file_name os.join('nookipedia', %s, %s+'.html')"%(key, title)
+			shutil.copy(file_name, os.path.join(base, key, title.replace(r'/', '_')+'.html'))
+
 def main(argv):
-	targetdir = {}#dict of array to filename tuples
+	targetdirs = {}#dict of array to filename tuples
 	#{'a': [('foo', 'path/to/foo.html')]}
 	
 	#init data
 	for i in range(26):
-		targetdir[chr(ord('a')+i)] = []
+		targetdirs[chr(ord('a')+i)] = []
 
 	print 'walking %s'%argv[1]
 	for root, dirs, files in os.walk(argv[1]):
@@ -43,12 +54,13 @@ def main(argv):
 			key = title[0].lower()
 			print "key=", key
 			if not title: raise "Could not get title for %s" % os.path.join(root,f)
-			if key not in targetdir:
+			if key not in targetdirs:
 				print "key not in target dir"
 				continue
-			targetdir[key].append((title, os.path.join(root, f)))
+			targetdirs[key].append((title, os.path.join(root, f)))
 
-	print repr(targetdir)
+	print repr(targetdirs)
+	create_files(targetdirs)
 
 
 
